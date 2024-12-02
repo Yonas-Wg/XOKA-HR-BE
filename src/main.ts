0b79163f6f1177b44d954e6b4e358c111c6fe8ef
Yonas-Wg <yonas@nilesync.com>
2024-12-02 11:08:53 +0300

    serverless deploy

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import serverless from 'serverless-http';
+
+let cachedHandler: any; // Caching the handler for re-use in serverless environments
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Enable CORS for your frontend domain
   app.enableCors({
-    origin: 'https://xoka-hr-management.vercel.app', 
+    origin: 'https://xoka-hr-management.vercel.app',
     methods: 'GET, POST, PUT, DELETE', // Allowed HTTP methods
     allowedHeaders: 'Content-Type, Accept', // Allowed headers
   });
@@ -17,13 +20,19 @@ async function bootstrap() {
     .setTitle('API Documentation')
     .setDescription('The API description for HR-Management-System application')
     .setVersion('1.0')
-    
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document); // You can access Swagger docs at http://localhost:3000/api
+  SwaggerModule.setup('api', app, document); // Swagger docs available at /api
 
-  await app.listen(3000); 
+  await app.init();
+  return app.getHttpAdapter().getInstance();
 }
 
-bootstrap();
+export const handler = async (event: any, context: any) => {
+  if (!cachedHandler) {
+    const appInstance = await bootstrap();
+    cachedHandler = serverless(appInstance);
+  }
+  return cachedHandler(event, context);
+};
